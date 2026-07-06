633448dcda32ef406a6ed420306e2afe06c34e51
Quanayzia Garden <86074391+gardenqu@users.noreply.github.com>
2026-07-06 18:03:27 -0400

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1;     // Pointer for end of valid nums1 elements
+        int j = n - 1;     // Pointer for end of nums2
+        int k = m + n - 1; // Pointer for the very end of nums1 array
+
+        // Merge from right to left
+        while (i >= 0 && j >= 0) {  
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If there are remaining elements in nums2, copy them over.
+        // (If nums1 has remaining elements, they are already in the right place!)
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+
+    }
+}
